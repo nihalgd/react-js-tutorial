90c10ad7cd1a3747b5101992a6b1605e4b414a52
nihalgd <nihalgoud.12345@gmail.com>
2026-03-13 01:34:18 +0530

    Learn React-JS

---
+++ 04-cards-project/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from './components/cards'
+
+const App = () => {
+  return (
+    <div className='parent'>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+    </div>
+  )
+}
+
+export default App
