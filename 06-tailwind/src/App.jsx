69e1e01cfbe53bddc91f011d1353d2e5f5dd3dfe
nihalgd <nihalgoud.12345@gmail.com>
2026-03-14 17:08:06 +0530

    Learn React-JS

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,8 @@
+
+const App = () => {
+  return (
+    <div className="h-full w-full text-red-500 bg-amber-400" >Tailwind CSS!</div>
+  )
+}
+
+export default App
---
+++ 07-ui-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black text-shadow-red text-white'>
+      <h1>07-UI PROJECT</h1>
+    </div>
+  )
+}
+
+export default App
