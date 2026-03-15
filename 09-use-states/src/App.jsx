bb932f1866df65b0238a7267f23f6698348c9320
nihalgd <nihalgoud.12345@gmail.com>
2026-03-16 00:42:57 +0530

    Learn React-JS

--- 09-use-states/src/App.jsx
+++ 09-use-states/src/App.jsx
@@ -1,26 +1,14 @@
-import { useState } from "react"
-import 'remixicon/fonts/remixicon.css'
-
+import React from 'react'
 
 const App = () => {
-  const [kitna, setfirst] = useState("?")
-  const [kitnaa, set] = useState("0")
-  function dek (){
-    setfirst("02")
-
-  }
-  function inc(){
-    set(kitnaa + 1)
-  }
-   function dec(){
-    set(kitnaa - 1)
-  }
-   function plus(){
-    set(kitnaa + 5)
-  }
   return (
-    <div>
-      <h1 id="le"><i className="ri-infinity-line"></i></h1>
+    <div>App</div>
+  )
+}
+
+export default App
+
+ {/* <h1 id="le"><i className="ri-infinity-line"></i></h1>
       <h1> 1 + 1 = {kitna} </h1>
       <button onClick={dek}>calculate</button>
 
@@ -29,10 +17,20 @@ const App = () => {
         <button onClick={inc}>+</button>
         <button onClick={dec}>-</button>
         <button onClick={plus}>+5</button>
-      </div>
-    </div>
-    
-  )
-}
+      </div> */}
 
-export default App
+      // const [kitna, setfirst] = useState("?")
+  // const [kitnaa, set] = useState("0")
+  // function dek (){
+  //   setfirst("02")
+
+  // }
+  // function inc(){
+  //   set(kitnaa + 1)
+  // }
+  //  function dec(){
+  //   set(kitnaa - 1)
+  // }
+  //  function plus(){
+  //   set(kitnaa + 5)
+  // }
