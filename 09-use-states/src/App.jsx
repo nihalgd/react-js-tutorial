1dada21329be24232b840b0fdab5eca4c81b7517
nihalgd <nihalgoud.12345@gmail.com>
2026-03-16 00:34:52 +0530

    Learn React-JS

--- 09-use-states/src/App.jsx
+++ 09-use-states/src/App.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import { useState } from "react"
+import 'remixicon/fonts/remixicon.css'
+
 
 const App = () => {
+  const [kitna, setfirst] = useState("?")
+  const [kitnaa, set] = useState("0")
+  function dek (){
+    setfirst("02")
+
+  }
+  function inc(){
+    set(kitnaa + 1)
+  }
+   function dec(){
+    set(kitnaa - 1)
+  }
+   function plus(){
+    set(kitnaa + 5)
+  }
   return (
-    <div>App</div>
+    <div>
+      <h1 id="le"><i className="ri-infinity-line"></i></h1>
+      <h1> 1 + 1 = {kitna} </h1>
+      <button onClick={dek}>calculate</button>
+
+      <div>
+        <h2>{kitnaa}</h2>
+        <button onClick={inc}>+</button>
+        <button onClick={dec}>-</button>
+        <button onClick={plus}>+5</button>
+      </div>
+    </div>
+    
   )
 }
 
