b54b1f2e29adfd45c35a42fded0b1a9b0344b50a
nihalgd <nihalgoud.12345@gmail.com>
2026-03-17 15:32:45 +0530

    Learn React-JS

--- 11-form-handling/src/App.jsx
+++ 11-form-handling/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
 const App = () => {
+  const form = (o) => {
+    console.log("form is Sumbited!")
+    o.preventDefault()
+    
+  }
   return (
-    <div>Form Handling Bolteyyy!!!</div>
+    <div>
+      <form onSubmit={ (o) => {
+        form(o)
+      }
+      }>
+        <input type="text" placeholder='Enter your Name' />
+        <button>Sumbit</button>
+      </form>
+    </div>
   )
 }
 
