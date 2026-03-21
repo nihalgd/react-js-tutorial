841c90e430a7baba648800dffed8b7fd982fdc65
nihalgd <nihalgoud.12345@gmail.com>
2026-03-22 00:46:09 +0530

    Learn React-JS

--- 17-gallery-project/src/App.jsx
+++ 17-gallery-project/src/App.jsx
@@ -1,7 +1,17 @@
+import axios from 'axios'
+
 
 const App = () => {
+
+  const data = async ()=>{
+  const getData = await axios.get('https://picsum.photos/v2/list?page=2&limit=10')
+  console.log(getData);
+  }
   return (
-    <div>App</div>
+    <div className="h-screen bg-black text-white">
+
+      <button onClick={data} className="bg-black text-white p-5 m-10 border-4">Get Data!</button>
+    </div>
   )
 }
 
