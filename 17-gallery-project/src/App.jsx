754ef536b0dad55d7a8059a3a8c4d59e30f3260e
nihalgd <nihalgoud.12345@gmail.com>
2026-03-22 17:47:37 +0530

    Learn React-JS

--- 17-gallery-project/src/App.jsx
+++ 17-gallery-project/src/App.jsx
@@ -1,16 +1,24 @@
 import axios from 'axios'
+import { useState } from 'react'
 
 
 const App = () => {
+  const arr = [{},{},{}]
+
+  const [Userdata, setUserdata] = useState([])
 
   const data = async ()=>{
-  const getData = await axios.get('https://picsum.photos/v2/list?page=2&limit=10')
-  console.log(getData);
+  const responce = await axios.get('https://picsum.photos/v2/list?page=2')
+  setUserdata(responce.data);
+  console.log(responce.data);
+  
   }
   return (
+
     <div className="h-screen bg-black text-white">
 
       <button onClick={data} className="bg-black text-white p-5 m-10 border-4">Get Data!</button>
+
     </div>
   )
 }
